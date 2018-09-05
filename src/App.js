a18055b2912a720eb2a348fe3c0e23ff6f08d943
“Richard Middleton” <Richard@richardmiddleton.me>
2018-09-05 13:37:06 +0200

    samples working and function passing

--- src/App.js
+++ src/App.js
@@ -9,20 +9,27 @@ class Pad extends Component {
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyPress);
   }
-  handleKeyPress(e) {
-     console.log(e.key)
+  handleKeyPress = (e) => {
+    const pressed = e.key.toUpperCase();
+    if (pressed === this.props.stuff.keyTrigger) {
+      this.playSample();
+    }
   }
   
-  playSound(e) {
-    const audio = e.target.children[0];
+  playSample = () => { 
+    const audio = document.getElementById(this.props.stuff.keyTrigger);
     audio.currentTime = 0;
     audio.play();
+  }
+
+  callSample = () => {
+    this.props.playSample(this.props.stuff.keyTrigger);
   }
 
   render() {
     const { stuff } = this.props;
     return (
-        <div className="drum-pad" onClick={this.playSound}>
+        <div className="drum-pad" onClick={this.callSample}>
 					{stuff.keyTrigger}
 					<audio className='clip' id={stuff.keyTrigger} src={stuff.url}></audio>
 				</div>
@@ -32,6 +39,33 @@ class Pad extends Component {
 }
 
 class Padbank extends Component {
+
+  render() {
+    const pads = this.props.keys.map(item => (
+      <Pad playSample={this.props.playSample} key={item.keyTrigger} stuff={item}/>
+    ));
+    return(
+    <div className="pad-container">
+      {pads}
+    </div>
+    ) 
+  }
+}
+
+
+class Display extends Component {
+	render() {
+		return (
+      <div id="display" className="display">
+        <div className="lcd">
+
+        </div>
+      </div>
+    )
+    }
+  }
+
+class App extends Component {
 
   state = {
     keys: [
@@ -84,24 +118,18 @@ class Padbank extends Component {
   }
 
 
-  render() {
-    const items = this.state.keys.map(item => (
-      <Pad key={item.keyTrigger} stuff={item}/>
-    ));
-    return(
-    <div className="pad-container">
-      {items}
-    </div>
-    ) 
+  playSample = (e) => {
+    const audio = document.getElementById(e);
+    audio.currentTime = 0;
+    audio.play();
   }
-}
 
-class App extends Component {
 	render() {
 		return (
 			<div id="drum-machine" className="drum-machine">
-        <Padbank />
-				<div id="display" className="display" />
+        <Padbank playSample={this.playSample} keys={this.state.keys} />
+        <Display />
+
 			</div>
 		);
 	}
